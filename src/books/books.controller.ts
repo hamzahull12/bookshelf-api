ca2efa4efe4a5920d31b926f7de3df318faa3ce9
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2026-01-31 01:45:09 +0700

    create service and controller for add book and get book

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createBook(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.addBook(createBookDto);
+  }
+
+  @Get()
+  findAllNote() {
+    return this.booksService.getAllBooks();
+  }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Book } from './interfaces/book.interface';
+import { CreateBookDto } from './dto/book.dto';
+import { nanoid } from 'nanoid';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  private books: Book[] = [];
+
+  addBook(createBookDto: CreateBookDto) {
+    const { name, readPage, pageCount } = createBookDto;
+
+    if (!name) {
+      throw new HttpException(
+        {
+          status: 'fail',
+          message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    if (readPage > pageCount) {
+      throw new HttpException(
+        {
+          status: 'fail',
+          message:
+            'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const id = nanoid(16);
+    const timestamp = new Date().toISOString();
+
+    const newBook: Book = {
+      id,
+      ...createBookDto,
+      finished: readPage === pageCount,
+      insertedAt: timestamp,
+      updatedAt: timestamp,
+    };
+    this.books.push(newBook);
+
+    return {
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    };
+  }
+
+  getAllBooks() {
+    const booksData = this.books.map((book) => ({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher,
+    }));
+
+    return {
+      status: 'success',
+      data: {
+        books: booksData,
+      },
+    };
+  }
+}
