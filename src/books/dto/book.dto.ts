f0a017bf07101b1f826c08424e2991fc400146e8
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2026-01-31 00:22:10 +0700

    create dto and interface

---
+++ src/books/dto/book.dto.ts
@@ -0,0 +1,10 @@
+export class CreateBookDto {
+  name: string;
+  year: number;
+  author: string;
+  summary: string;
+  publisher: string;
+  pageCount: number;
+  readPage: number;
+  reading: boolean;
+}
